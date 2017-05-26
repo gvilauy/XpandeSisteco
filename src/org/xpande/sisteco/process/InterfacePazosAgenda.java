77efc15a4b2547cc3d059dfebea8691794cbeea6
gvilauy <gabrielvila13@gmail.com>
2017-05-26 15:47:54 -0300

    Initial commit

---
+++ src/org/xpande/sisteco/process/InterfacePazosAgenda.java
@@ -0,0 +1,8 @@
+package org.xpande.sisteco.process;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay (LUY).
+ * OpenUp. Created by gabriel on 5/26/17.
+ */
+public class InterfacePazosAgenda {
+}
