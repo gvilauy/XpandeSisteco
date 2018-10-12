31167214dea88da908b75c43513abb658794c43b
gabriel <gabrielvila13@gmail.com>
2018-10-11 23:31:29 -0300

    OK
    Signed-off-by: gabriel <gabrielvila13@gmail.com>

--- src/org/xpande/sisteco/process/GeneradorProdHexa.java
+++ src/org/xpande/sisteco/process/GeneradorProdHexa.java
@@ -30,15 +30,24 @@ public class GeneradorProdHexa extends SvrProcess {
         ResultSet rs = null;
 
         try{
+
+            /*
             sql = " select m_product_id " +
                     " from m_product " +
                     " where isactive ='Y' " +
                     " and issold ='Y' " +
                     " and atributoshexa is null ";
+            */
 
-        	pstmt = DB.prepareStatement(sql, null);
+            sql = " select m_product_id " +
+                    " from m_product " +
+                    " where isactive ='Y' " +
+                    " and issold ='Y' ";
+
+            pstmt = DB.prepareStatement(sql, null);
         	rs = pstmt.executeQuery();
 
+        	/*
         	while(rs.next()){
 
                 // Me aseguro que el producto tenga atributos asociados, sino se los creo ahora.
@@ -61,6 +70,31 @@ public class GeneradorProdHexa extends SvrProcess {
                     }
                 }
             }
+            */
+
+            String action = "";
+
+            while(rs.next()){
+
+                // Elimino atributos del producto y los genero nuevamente
+                action = " delete from z_productoatribsisteco where m_product_id =" + rs.getInt("m_product_id");
+                DB.executeUpdateEx(action, null);
+
+                MProduct product = new MProduct(getCtx(), rs.getInt("m_product_id"), null);
+
+                // Asociación de atributos que requiere Sisteco al nuevo producto
+                SistecoUtils.setProductAttributes(getCtx(), product, null);
+
+                // Obtengo y guardo valor Hexadecimal segun seteos de atributos para el producto recibido.
+                // Solo si el producto tiene unidad de medida o tandem asociado.
+                if ((product.getC_UOM_ID() > 0) || (product.get_ValueAsInt("M_Product_Tandem_ID") > 0)){
+                    String valorHexadecimal = SistecoUtils.getHexadecimalAtributos(getCtx(), product, get_TrxName());
+                    action = " update m_product set atributoshexa ='" + valorHexadecimal + "' " +
+                            " where m_product_id =" + product.get_ID();
+                    DB.executeUpdateEx(action, null);
+                }
+            }
+
         }
         catch (Exception e){
             throw new AdempiereException(e);
